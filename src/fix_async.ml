71390a13335d0c12e136403651a98c1c24c32e98
Vincent Bernardoff <vb@luminar.eu.org>
2019-02-05 19:02:28 +0100

    add LICENSE

--- src/fix_async.ml
+++ src/fix_async.ml
@@ -1,3 +1,8 @@
+(*---------------------------------------------------------------------------
+   Copyright (c) 2019 Vincent Bernardoff. All rights reserved.
+   Distributed under the ISC license, see terms at the end of the file.
+  ---------------------------------------------------------------------------*)
+
 open Rresult
 open Core
 open Async
@@ -193,3 +198,19 @@ let with_connection_ez
   don't_wait_for (cleanup ()) ;
   don't_wait_for (Pipe.closed ww >>| Ivar.fill_if_empty do_logout) ;
   return (Ivar.read closed, r, ww)
+
+(*---------------------------------------------------------------------------
+   Copyright (c) 2019 Vincent Bernardoff
+
+   Permission to use, copy, modify, and/or distribute this software for any
+   purpose with or without fee is hereby granted, provided that the above
+   copyright notice and this permission notice appear in all copies.
+
+   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+   WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+   MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+   ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+   WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+   ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+   OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+  ---------------------------------------------------------------------------*)
